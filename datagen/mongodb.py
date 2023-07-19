3f65bcb40c8d91aa7d6949ac86c04ec893e7ccc1
nelsonad-ops <nelson.andrew@gmail.com>
2023-07-19 09:18:21 -0700

    initial

---
+++ datagen/mongodb.py
@@ -0,0 +1,13 @@
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+import pandas as pd
+from modules.dataframe import create_dataframe
+from modules.base import client
+if __name__ == "__main__":
+    num_cores = cpu_count() - 1
+    with Pool() as pool:
+        data = pd.concat(pool.map(create_dataframe, range(num_cores)))
+    data_dict = data.to_dict('records')
+    db = client["company"]
+    collection = db["employees"]
+    collection.insert_many(data_dict)
